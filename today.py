92f1fb20fa208228b8c4a25a46ed871e80c03e20
Kashif Qureshi <167954723+Kaashifqureshi@users.noreply.github.com>
2026-07-16 11:13:18 +0500

    Update today.py

--- today.py
+++ today.py
@@ -10,7 +10,7 @@ import hashlib
 # Account permissions: read:Followers, read:Starring, read:Watching
 # Repository permissions: read:Commit statuses, read:Contents, read:Issues, read:Metadata, read:Pull Requests
 HEADERS = {'authorization': 'token '+ os.environ['ACCESS_TOKEN']}
-USER_NAME = os.environ['USER_NAME'] # 'Kaashifqureshi'
+USER_NAME = os.environ['Kaashifqureshi'] # 'Kaashifqureshi'
 QUERY_COUNT = {'user_getter': 0, 'follower_getter': 0, 'graph_repos_stars': 0, 'recursive_loc': 0, 'graph_commits': 0, 'loc_query': 0}
 
 
